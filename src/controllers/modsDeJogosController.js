98b1c8d54538ec1880ab36b1c4dba54d133163d0
Murilo <murilo.r.rocha@aluno.senai.br>
2025-09-22 10:35:29 -0300

    Cria createMod

--- src/controllers/modsDeJogosController.js
+++ src/controllers/modsDeJogosController.js
@@ -60,6 +60,73 @@ const getById = (req, res) => {
     })
 }
 
+const createMod = (req, res) => {
+    const { nomeMod, jogo, autor, versao, downloads, categoria, compatibilidade } = req.body;
+
+    if (!nomeMod) {
+        return res.status(400).json({
+            success: false,
+            message: "O campo 'nomeMod' é obrigatório para criar uma mod!"
+        });
+    }
+
+    if (!jogo) {
+        return res.status(400).json({
+            success: false,
+            message: "O campo 'jogo' é obrigatório!"
+        });
+    }
+
+    if (!autor) {
+        return res.status(400).json({
+            success: false,
+            message: `O campo 'autor' é obrigatório!`
+        });
+    }
+
+    if (!downloads) {
+        return res.status(400).json({
+            success: false,
+            message: "O campo 'downloads' é obrigatório para criar uma mod!"
+        });
+    }
+
+    if (!versao) {
+        return res.status(400).json({
+            success: false,
+            message: `O campo 'versao' é obrigatório!`
+        });
+    }
+
+    if (!compatibilidade) {
+        return res.status(400).json({
+            success: false,
+            message: "O campo 'compatibilidade' é obrigatório para criar uma mod!"
+        });
+    }
+
+
+
+    const novoMod = {
+        id: mods.length + 1,
+        nomeMod,
+        jogo,
+        autor: autor.toLowerCase(),
+        versao: versao.toLowerCase(),
+        downloads,
+        dataLancamento: new Date(),
+        categoria,
+        compatibilidade
+    }
+
+    mods.push(novoMod);
+    res.status(201).json({
+        success: true,
+        message: "Mod criado com sucesso!",
+        mod: novoMod
+    })
+}
+
 
 
 export { getAllMods, getById, createMod, deleteMod };
